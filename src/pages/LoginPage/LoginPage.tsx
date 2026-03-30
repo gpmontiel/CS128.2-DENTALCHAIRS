3ff039c6b00abfb24a4747a170627a239e289082
gpmontiel <gpmontiel@up.edu.ph>
2026-03-31 00:19:29 +0800

    completed login page frontend

--- src/pages/LoginPage/LoginPage.tsx
+++ src/pages/LoginPage/LoginPage.tsx
@@ -1,15 +1,22 @@
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { supabase } from "../../utils/supabase.ts";
+import {Container, Row, Col, Form, Button, InputGroup, Toast, ToastBody, ToastContainer} from "react-bootstrap";
+import { FaEnvelope, FaExclamationTriangle } from "react-icons/fa";
+import { IoMdEye, IoMdEyeOff } from "react-icons/io";
+import "./LoginPage.css"
 
 interface LoginPageProps {
     onLoginSuccess: (profile: any) => void;
 }
 
 const LoginPage = ({ onLoginSuccess }: LoginPageProps) => {
+    const navigate = useNavigate();
     const [email, setEmail] = useState("");
     const [password, setPassword] = useState("");
-    const navigate = useNavigate();
+    const [showPassword, setShowPassword] = useState(false);
+    const [showToast, setShowToast] = useState(false);
+    const [toastMessage, setToastMessage] = useState("");
 
     const handleLogin = async (e: React.SyntheticEvent<HTMLFormElement>) => {
         e.preventDefault();
@@ -23,7 +30,9 @@ const LoginPage = ({ onLoginSuccess }: LoginPageProps) => {
         });
 
         if (error || !data.user) {
-            alert("Login failed: " + error?.message);
+            console.log("Login failed:", error?.message);
+            setToastMessage("Incorrect username or password.");
+            setShowToast(true);
             return;
         }
 
@@ -37,7 +46,8 @@ const LoginPage = ({ onLoginSuccess }: LoginPageProps) => {
             .single();
 
         if (profileError || !profile) {
-            alert("Failed to fetch user profile.");
+            setToastMessage("Failed to fetch user profile.");
+            setShowToast(true);
             return;
         }
 
@@ -51,31 +61,162 @@ const LoginPage = ({ onLoginSuccess }: LoginPageProps) => {
         else alert("Unknown role");
     };
 
+    const togglePasswordVisibility = () => {
+        setShowPassword(prevShowPassword => !prevShowPassword);
+    };
+
     return (
-        <div className="login-container">
-            <h1>Login</h1>
-            <form onSubmit={handleLogin}>
-                <div>
-                    <label htmlFor="username">Email</label>
-                    <input
-                        type="text"
-                        value={email}
-                        onChange={(e) => setEmail(e.target.value)}
-                        required
-                    />
-                </div>
-                <div>
-                    <label htmlFor="password">Password</label>
-                    <input
-                        type="password"
-                        value={password}
-                        onChange={(e) => setPassword(e.target.value)}
-                        required
-                    />
-                </div>
-                <button type="submit">Login</button>
-            </form>
-        </div>
+        <Container fluid style={{ backgroundColor: "#4A3878", minHeight: "100vh", padding: 0, fontFamily: "Inter, sans-serif" }}>
+
+            {/* toast notification */}
+            <ToastContainer className="p-3" position="top-center">
+                <Toast
+                    onClose={() => setShowToast(false)}
+                    show={showToast}
+                    delay={10000}
+                    autohide
+                    style={{
+                        backgroundColor: "#FFF4E5",
+                        boxShadow: "0 4px 12px rgba(0,0,0,0.15)",
+                        borderRadius: "8px",
+                    }}
+                >
+                    <ToastBody className="d-flex align-items-center justify-content-center gap-3">
+                        <FaExclamationTriangle style={{ color: "#EF7B2B" }} size={20} />
+                        <span style={{ color: "#663C01", fontWeight: "500"}}>
+                            {toastMessage}
+                        </span>
+                    </ToastBody>
+                </Toast>
+            </ToastContainer>
+
+            <Row className="justify-content-center pt-3 px-3" style={{ margin: 0, minHeight: "100vh" }}>
+                <Col xs={12} sm={12} md={9} lg={6} xl={5} style={{ padding: 0, display: "flex", flexDirection: "column" }}>
+                    <div className="text-center mb-4" style={{ paddingTop: "2rem" }}>
+                        <div
+                            style={{
+                                display: "flex",
+                                alignItems: "center",
+                                justifyContent: "center",
+                                gap: "10px",
+                            }}
+                        >
+                            <img src="src/assets/logo-light.png" alt="Logo" style={{ width: "50px" }} />
+                            <h1 style={{
+                                    color: "#E9B0F8",
+                                    fontFamily: "Poppins, sans-serif",
+                                    fontWeight: 700,
+                                    margin: 0,
+                                    fontSize: "2.5rem",
+                                }}
+                            >
+                                DenTrack
+                            </h1>
+                        </div>
+
+                        <p className="text-center mx-4"
+                            style={{
+                                color: "white",
+                                fontSize: "0.75rem",
+                                fontWeight: 500,
+                                marginTop: "20px",
+                                marginBottom: "15px",
+                            }}
+                        >
+                            Dental Chair Assignment & Student Attendance
+                            Monitoring and Management System
+                        </p>
+                    </div>
+
+                    <style>
+                        {`
+                        /* mobile (default) */
+                        .login-card-mobile {
+                            background-color: white;
+                            border-top-left-radius: 30px;
+                            border-top-right-radius: 30px;
+                            border-bottom-left-radius: 0;
+                            border-bottom-right-radius: 0;
+                            flex: 1;
+                            display: flex;
+                            flex-direction: column;
+                            justify-content: flex-start;
+                        }
+          
+                        /* desktop */
+                        @media (min-width: 800px) {
+                            .login-card-mobile {
+                                background-color: #FAF4FF !important;
+                                border-radius: 20px !important;
+                                margin: 0 1rem !important;
+                                min-height: auto !important;
+                                box-shadow: 0 4px 12px rgba(0,0,0,0.1) !important;
+                                padding: 1.5rem !important;
+                                flex: none !important;
+                            }
+                        }
+                    `}
+                    </style>
+
+                    <div className="login-card-mobile px-3 px-lg-0">
+                        <div style={{ maxWidth: "500px", margin: "0 auto", width: "100%", padding: "2rem 1rem" }}>
+                            <h1
+                                className="fw-bold text-center mb-1 mt-1"
+                                style={{ fontSize: "2.3rem" }}
+                            >
+                                Welcome Back!
+                            </h1>
+                            <p className="text-center mb-4" style={{ fontSize: "0.8rem", color: "#555" }}>
+                                Please enter your login credentials
+                            </p>
+
+                            <Form onSubmit={handleLogin}>
+                                <Form.Group className="mb-3">
+                                    <Form.Label style={{ fontWeight: "600"}}>Email</Form.Label>
+                                    <InputGroup>
+                                        <Form.Control required
+                                                      type="email"
+                                                      placeholder="Enter your email"
+                                                      value={email}
+                                                      onChange={(e) => setEmail(e.target.value)}
+                                        />
+                                        <InputGroup.Text style={{ backgroundColor: "white" }}>
+                                            <FaEnvelope color="#4A3878" />
+                                        </InputGroup.Text>
+                                    </InputGroup>
+                                </Form.Group>
+
+                                <Form.Group className="mb-5">
+                                    <Form.Label style={{ fontWeight: "600"}}>Password</Form.Label>
+                                    <InputGroup>
+                                        <Form.Control required
+                                                      type={showPassword ? "text" : "password"}
+                                                      placeholder="Enter your password"
+                                                      value={password}
+                                                      onChange={(e) => setPassword(e.target.value)}
+                                        />
+                                        <InputGroup.Text
+                                            style={{ backgroundColor: "white", cursor: "pointer" }}
+                                            onClick={togglePasswordVisibility}
+                                        >
+                                            {showPassword ? (
+                                                <IoMdEyeOff color="#4A3878" size={20} />
+                                            ) : (
+                                                <IoMdEye color="#4A3878" size={20} />
+                                            )}
+                                        </InputGroup.Text>
+                                    </InputGroup>
+                                </Form.Group>
+
+                                <Button type="submit" className="w-100 login-btn">
+                                    Login
+                                </Button>
+                            </Form>
+                        </div>
+                    </div>
+                </Col>
+            </Row>
+        </Container>
     );
 };
 
